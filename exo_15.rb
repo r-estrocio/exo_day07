40449d2b92ad95e37300879a67eb280e8d9e8d48
Raquel Estrocio <r.estrocio@gmail.com>
2022-04-12 21:18:35 +0200

    Adding file exo_15

--- exo_15.rb
+++ exo_15.rb
@@ -2,18 +2,18 @@ puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print "> "
 
 number_etages = Integer(gets.chomp)
-if number > 25
+if number_etages > 25
   number_etages = 25
 end
 
-if number < 1
+if number_etages < 1
   number_etages = 1
 end
 
 puts "Voici la pyramide :"
 
-number.times do |i|
-  for g in 0..i
+number_etages.times do |i|
+  (i + 1).times do
     print "#"
   end
   puts
