0d7f2289243a89a9758d51008c178b5b96fa9a32
Raquel Estrocio <r.estrocio@gmail.com>
2022-04-12 20:51:44 +0200

    Adding exo_13

--- exo_13.rb
+++ exo_13.rb
@@ -1,13 +1,12 @@
 # "jean.dupont.01@email.fr"
 # "jean.dupont.02@email.fr"
 # etc..
-emails = []
+
 1.upto(50) do |i|
   if i < 10
     index = "0#{i}"
   else
     index = i.to_s
   end
-  email = "jean.dupont.#{index}@email.fr"
+  puts "jean.dupont.#{index}@email.fr"
 end
-puts emails
