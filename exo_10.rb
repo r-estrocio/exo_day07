8cfb39424b052edf2189504e16dc6a7b4dd19682
Raquel Estrocio <r.estrocio@gmail.com>
2022-04-12 12:16:27 +0200

    exo_10

---
+++ exo_10.rb
@@ -0,0 +1,29 @@
+# demande l'année de naissance
+puts "Quelle est votre anée de naissance ?"
+print "> "
+
+birth_year = Integer(gets.chomp)
+year_now = Time.now.year
+year = birth_year + 1
+
+while year <= year_now
+  age = year - birth_year
+  if year == year_now
+    if age == 1
+      puts "Cette année vous avez un an."
+    else
+      puts "Cette année vous avez #{age} ans."
+    end
+  else
+    if age == 1
+      puts "En #{year} vous aviez un an."
+    else
+      puts "En #{year} vous aviez #{age} ans."
+    end
+  end
+  year = year + 1
+end
+
+# birth_year.upto(year_now) do |i|
+#   puts i
+# end
