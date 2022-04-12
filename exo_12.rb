0543bb39e8568b0c4f43c0bf9ce0851f4f507b15
Raquel Estrocio <r.estrocio@gmail.com>
2022-04-12 13:18:32 +0200

    exo_12

---
+++ exo_12.rb
@@ -0,0 +1,33 @@
+# demande l'année de naissance
+puts "Quelle est votre anée de naissance ?"
+print "> "
+
+birth_year = Integer(gets.chomp)
+year_now = Time.now.year
+year = birth_year + 1
+
+while year <= year_now
+  age = year - birth_year
+  years = year_now - year
+  if year == year_now
+    if age == 1
+      puts "Cette année vous avez un an."
+    else
+      puts "Cette année vous allez avoir #{age} ans."
+    end
+  else
+    if age == 1
+      puts "Il y a #{years} vous aviez un an."
+    elsif age == years
+      puts "Il y a #{years} ans, vous aviez la moitié de l'âge que vous avez aujourd'hui"
+    else
+      puts "Il y a #{years} vous aviez #{age} ans."
+    end
+  end
+  year = year + 1
+  years = year_now - year
+end
+
+# birth_year.upto(year_now) do |i|
+#   puts i
+# end
